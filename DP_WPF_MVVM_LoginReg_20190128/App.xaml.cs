3f096d38f8dd68f185896270d3a4b4588efd035e
rom3053 <70435537+rom3053@users.noreply.github.com>
2021-02-06 18:11:39 +0200

    Add files via upload

---
+++ DP_WPF_MVVM_LoginReg_20190128/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DP_WPF_MVVM_LoginReg_20190128
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ DP_WPF_MVVM_LoginReg_20190128/ViewModelLocator.cs
@@ -0,0 +1,115 @@
+﻿using DP_WPF_MVVM_LoginReg_20190128.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP_WPF_MVVM_LoginReg_20190128
+{
+    public class ViewModelLocator
+    {
+        private static UsersOverviewViewModel usersOverviewViewModel
+            = new UsersOverviewViewModel();
+        private static UserDetailViewModel userDetailViewModel =
+            new UserDetailViewModel();
+        private static CreateTestViewModel createTestViewModel =
+            new CreateTestViewModel();
+        private static TestBrowserViewModel testBrowserViewModel =
+            new TestBrowserViewModel();
+        private static PassingTheTestViewModel passingTheTestViewModel =
+            new PassingTheTestViewModel();
+        private static TestStatisticBrowserViewModel testStatisticBrowser =
+            new TestStatisticBrowserViewModel();
+        private static TwoListBoxesGroupSelectionViewModel twoListViewModel =
+            new TwoListBoxesGroupSelectionViewModel();
+        private static CreateCategoryViewModel createCategory =
+            new CreateCategoryViewModel();
+        private static GroupsOverviewViewModel groupsOverview =
+            new GroupsOverviewViewModel();
+        public static CreateTestViewModel CreateTestViewModel
+        {
+            get
+            {
+                return createTestViewModel;
+            }
+        }
+        public static UserDetailViewModel UserDetailViewModel
+        {
+            get
+            {
+                return userDetailViewModel;
+            }
+        }
+
+        public static UsersOverviewViewModel UsersOverviewViewModel
+        {
+            get
+            {
+                return usersOverviewViewModel;
+            }
+        }
+        public static TestBrowserViewModel TestBrowserViewModel
+        {
+            get
+            {
+                return testBrowserViewModel;
+            }
+        }
+        public static PassingTheTestViewModel PassingTheTestViewModel
+        {
+            set
+            {
+                passingTheTestViewModel = value;
+            }
+            get
+            {
+                return passingTheTestViewModel;
+            }
+        }
+        public static TestStatisticBrowserViewModel TestStatisticBrowserViewModel
+        {
+            set
+            {
+                testStatisticBrowser = value;
+            }
+            get
+            {
+                return testStatisticBrowser;
+            }
+        }
+        public static TwoListBoxesGroupSelectionViewModel TwoListBoxesGroupSelectionViewModel
+        {
+            set
+            {
+                twoListViewModel = value;
+            }
+            get
+            {
+                return twoListViewModel;
+            }
+        }
+        public static CreateCategoryViewModel CreateCategoryViewModel
+        {
+            set
+            {
+                createCategory = value;
+            }
+            get
+            {
+                return createCategory;
+            }
+        }
+        public static GroupsOverviewViewModel GroupsOverviewViewModel
+        {
+            set
+            {
+                groupsOverview = value;
+            }
+            get
+            {
+                return groupsOverview;
+            }
+        }
+    }
+}
